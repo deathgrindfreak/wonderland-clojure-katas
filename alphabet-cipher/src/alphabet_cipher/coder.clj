7e1eafa034a485bd7a16b19a0ef7d28e3af0eefe
Cooper Bell <john.cooper.bell@gmail.com>
2015-03-31 18:03:42 -0500

    solved alphabet-cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,20 @@
 (ns alphabet-cipher.coder)
 
+(defn code-char [f key letter]
+  (let [offset (int \a)]
+    (char (+ (mod (f (- (int letter) offset)
+                     (- (int key) offset))
+                  26)
+             offset))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str
+         (map #(code-char + %1 %2)
+              (cycle (seq keyword))
+              (seq message))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str
+         (map #(code-char - %1 %2)
+              (cycle (seq keyword))
+              (seq message))))
